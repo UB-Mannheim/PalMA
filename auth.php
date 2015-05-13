33271b4c66797090c43a3c79e05f49ddfcb46293
Stefan Weil <sw@weilnetz.de>
2015-05-13 19:52:11 +0200

    Pass user selected language from base URL to login.php
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- auth.php
+++ auth.php
@@ -7,13 +7,18 @@
         if (isset($_SERVER['HTTP_REFERER'])) {
             error_log("auth.php referred by " . $_SERVER['HTTP_REFERER']);
         }
-        if (isset($_REQUEST['pin'])) {
-            header('Location: login.php?pin=' . $_REQUEST['pin']);
-        } else {
-            header('Location: login.php');
+        $header = 'Location: login.php';
+        $separator = '?';
+        if (isset($_REQUEST['lang'])) {
+            $header = $header . $separator . 'lang=' . $_REQUEST['lang'];
+            $separator = '&';
         }
+        if (isset($_REQUEST['pin'])) {
+            $header = $header . $separator . 'pin=' . $_REQUEST['pin'];
+            $separator = '&';
+        }
+        header($header);
 
-        // header('Location: http://'.$hostname.($path == '/' ? '' : $path).'/login.php');
         exit;
     }
 ?>
