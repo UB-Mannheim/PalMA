f9c7864eba6201c3b1e499cd04605a5c2fe013dd
Stefan Weil <sw@weilnetz.de>
2015-05-20 11:16:33 +0200

    Add simple demo theme
    This is a minimalistic theme which can be copied and extended
    for your own theme.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

---
+++ theme/demo/simple/README.md
@@ -0,0 +1,31 @@
+Simple demo theme for PalMA
+===========================
+
+This is a minimalistic theme which can be copied and extended
+for your own theme.
+
+It includes these files:
+
+* `background.png`.
+  This is the background image which users will see on the team display
+  when they work with PalMA. Any user windows will be shown on top of
+  this background image.
+
+* `screensaver.php`.
+  PalMA shows this URL when no users are connected. Here we have a HTML page
+  with dynamically updated information like the URL and the PIN.
+
+These optional files are missing in the simple demo theme:
+
+* `favicon.ico`.
+  This icon is typically shown in bookmark lists of browsers or when the
+  PalMA URL is saved on a smartphone.
+
+* `palma-logo-49x18.png`.
+  This logo is used by PalMA's web interface `index.php`.
+
+* `palma-logo-67x25.png`.
+  This logo is used by the login web interface `login.php`.
+
+* `winvnc-palma.exe`.
+  This is a preconfigured UltraVNC server for Windows.
---
+++ theme/demo/simple/screensaver.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+
+Copyright (C) 2015 Universitätsbibliothek Mannheim
+See file LICENSE for license details.
+
+Author: Stefan Weil, Universitätsbibliothek Mannheim
+
+*/
+
+require_once('../../../DBConnector.class.php');
+
+$servername = $_SERVER["SERVER_NAME"];
+$serveraddress = $_SERVER["SERVER_ADDR"];
+$serveruri = dirname(dirname(dirname(dirname($_SERVER["REQUEST_URI"]))));
+$pin = sprintf("%04u", rand(0, 9999));
+$url = "http://${servername}${serveruri}";
+
+// Store PIN in database.
+$dbcon = new DBConnector();
+$dbcon->exec("UPDATE setting SET value='$pin' WHERE key='pin'");
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>PalMA &ndash; Screen Saver</title>
+    <style type="text/css">
+        html {
+            font-family: DejaVu Sans,sans-serif;
+            font-weight: 100;
+            height: 100%;
+            width: 100%;
+            background-color: black;
+            color: white;
+            font-size: 250%;
+        }
+        td {
+            padding-right: 20px;
+        }
+        #box {
+            margin: 200px;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="box">
+    <table>
+        <tr>
+            <td>URL:</td>
+            <td><?=$url?></td>
+            <td rowspan="2">
+                <img id="QR-Code"
+                    src="../../../qrcode/php/qr_img.php?d=<?=$url?>?pin=<?=$pin?>&amp;e=H"
+                    alt="QR Code">
+            </td>
+        </tr>
+        <tr>
+            <td>PIN:</td>
+            <td><?=$pin?></td>
+        </tr>
+    </table>
+    </div>
+</body>
+</html>
