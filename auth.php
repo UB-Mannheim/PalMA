8ce2b68ca22a54b0addfa137b325d1b2e0f80bb4
Stefan Weil <sw@weilnetz.de>
2015-09-23 19:11:00 +0100

    auth: Use PSR-2 coding style
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- auth.php
+++ auth.php
@@ -1,23 +1,23 @@
 <?php
-    if (!isset($_SESSION)) {
-        session_start();
-    }
+if (!isset($_SESSION)) {
+    session_start();
+}
 
-    if (!isset($_SESSION['username'])) {
-        if (isset($_SERVER['HTTP_REFERER'])) {
-            error_log("auth.php referred by " . $_SERVER['HTTP_REFERER']);
-        }
-        $header = 'Location: login.php';
-        $separator = '?';
-        if (isset($_REQUEST['lang'])) {
-            $header = $header . $separator . 'lang=' . $_REQUEST['lang'];
-            $separator = '&';
-        }
-        if (isset($_REQUEST['pin'])) {
-            $header = $header . $separator . 'pin=' . $_REQUEST['pin'];
-            $separator = '&';
-        }
-        header($header);
-
-        exit;
+if (!isset($_SESSION['username'])) {
+    if (isset($_SERVER['HTTP_REFERER'])) {
+        error_log("auth.php referred by " . $_SERVER['HTTP_REFERER']);
     }
+    $header = 'Location: login.php';
+    $separator = '?';
+    if (isset($_REQUEST['lang'])) {
+        $header = $header . $separator . 'lang=' . $_REQUEST['lang'];
+        $separator = '&';
+    }
+    if (isset($_REQUEST['pin'])) {
+        $header = $header . $separator . 'pin=' . $_REQUEST['pin'];
+        $separator = '&';
+    }
+    header($header);
+
+    exit;
+}
