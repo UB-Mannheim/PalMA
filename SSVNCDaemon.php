UB-Mannheim/PalMA

commit 18fd7b640e6d2c28dc1c6decf1c1dbbd5ca1fa15

Set global constants from configuration file
The configuration file palma.ini must no longer be parsed by every code
which needs configuration entries.

More configuration settings are now optional and provide reasonable
default values.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

SSVNCDaemon.php
@@ -7,8 +7,6 @@ require_once("DBConnector.class.php");
 
 class SSVNCDaemon {
 
-private $_URL;
-
 // all during session connected VNC Clients
 private $_VNC_CLIENTS;
 
@@ -38,28 +36,9 @@ public function __construct() {
 
 protected function startVNCViewer() {
 
-    $display=":0";
-
-    if (file_exists('palma.ini')) {
-
-        // Get configuration from ini file.
-        $conf = parse_ini_file("palma.ini", true);
-        $this->_URL = $conf['path']['control_file'];
-
-        $display = $conf['display']['id'];
-        if (isset($conf['display']['ssh'])) {
-            $ssh = $conf['display']['ssh'];
-        }
-
-    } else {
-
-        // Guess configuration from global PHP variables.
-        $this->_URL = dirname($_SERVER['HTTP_REFERER']) . '/' .
-                        basename($_SERVER['PHP_SELF'] . '/control.php');
-    }
-
     // Startup SSVNC-Viewer in Multilisten-Mode
-    $cmd = "export DISPLAY=".$display."; killall -q ssvncviewer; ssvncviewer -viewonly -multilisten 0 2>&1";
+    $cmd = "export DISPLAY=" . CONFIG_DISPLAY .
+        "; killall -q ssvncviewer; ssvncviewer -viewonly -multilisten 0 2>&1";
     $handle = popen($cmd, 'r');
     print("[Daemon]: vnc_viewer started");
 
@@ -283,7 +262,7 @@ protected function sendVncWindowToNuc($id, $vncclient) {
         // Set some options - we are passing in a useragent too here
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $this->_URL.'?newVncWindow='.$sw,
+            CURLOPT_URL => CONFIG_CONTROL_FILE . '?newVncWindow=' . $sw,
             CURLOPT_USERAGENT => 'PalMA cURL Request'
             ));
         // Send the request & save response to $resp
@@ -338,7 +317,8 @@ protected function deleteInactiveVncWindow() {
 
             curl_setopt_array($curl, array(
                 CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_URL => $this->_URL.'?window=vncwin&delete=VNC&vncid='.$inactive_win_id,
+                CURLOPT_URL => CONFIG_CONTROL_FILE_URL .
+                    '?window=vncwin&delete=VNC&vncid=' . $inactive_win_id,
                 CURLOPT_USERAGENT => 'PalMA cURL Request'
                 ));
 

DBConnector.class.php
@@ -280,8 +280,81 @@ eod;
 
 }
 
+function set_constants() {
+    // Get some constants from a configuration file.
+
+    $conf = parse_ini_file("palma.ini");
+    //~ print_r($conf);
+
+    // Entries in group 'display'.
+    if (array_key_exists('id', $conf)) {
+        define('CONFIG_DISPLAY', $conf['id']);
+    }
+    if (array_key_exists('ssh', $conf)) {
+        define('CONFIG_SSH', $conf['ssh']);
+    }
+
+    // Entries in group 'general'.
+    if (array_key_exists('password', $conf)) {
+        define('CONFIG_PASSWORD', $conf['password']);
+    }
+    if (array_key_exists('pin', $conf)) {
+        define('CONFIG_PIN', $conf['pin']);
+    }
+    if (array_key_exists('stationname', $conf)) {
+        define('CONFIG_STATIONNAME', $conf['stationname']);
+    }
+    if (array_key_exists('theme', $conf)) {
+        define('CONFIG_THEME', $conf['theme']);
+    }
+
+    // Entries in group 'path'.
+    if (array_key_exists('control_file', $conf)) {
+        define('CONFIG_CONTROL_FILE', $conf['control_file']);
+    }
+    if (array_key_exists('start_url', $conf)) {
+        define('CONFIG_START_URL', $conf['start_url']);
+    }
+    if (array_key_exists('upload_dir', $conf)) {
+        define('CONFIG_UPLOAD_DIR', $conf['upload_dir']);
+    }
+
+    // Set default values for constants missing in the configuration file.
+    // There is no default value for CONFIG_CONTROL_FILE.
+    if (!defined('CONFIG_DISPLAY')) {
+        // By default we use X display :0.
+        define('CONFIG_DISPLAY', ':0');
+    }
+    if (!defined('CONFIG_PASSWORD')) {
+        // Enable password authentisation by default.
+        define('CONFIG_PASSWORD', true);
+    }
+    if (!defined('CONFIG_PIN')) {
+        // Enable PIN authentisation by default.
+        define('CONFIG_PIN', true);
+    }
+    // There is no default value for CONFIG_SSH.
+    // There is no default value for CONFIG_START_URL.
+    if (!defined('CONFIG_STATIONNAME')) {
+        // Use the host name as the default station name.
+        define('CONFIG_STATIONNAME', gethostname());
+    }
+    if (!defined('CONFIG_THEME')) {
+        // The default theme is demo/simple.
+        define('CONFIG_THEME', 'demo/simple');
+    }
+    if (!defined('CONFIG_UPLOAD_DIR')) {
+        // The default theme is /var/www/html/uploads.
+        define('CONFIG_UPLOAD_DIR', '/var/www/html/uploads');
+    }
+}
+
+set_constants();
+
 if ($unittest[__FILE__]) {
     // Run unit test.
+
+    print('CONFIG_THEME = ' . CONFIG_THEME . "\n");
 
     function dbModifiedCallback() {
         echo("Triggered callback\n");

control.php
@@ -15,7 +15,7 @@ if (isset($unittest)) {
 }
 $unittest[__FILE__] = !isset($_SERVER['SERVER_NAME']);
 
-// initialize database
+// Connect to database and get configuration constants.
 require_once('DBConnector.class.php');
 $db = new DBConnector();
 
@@ -23,35 +23,11 @@ if (!$unittest[__FILE__]) {
     trace("QUERY_STRING=" . $_SERVER['QUERY_STRING']);
 }
 
-if (file_exists('palma.ini') && !$unittest[__FILE__]) {
-    // Get configuration from ini file.
-    if (isset($_SERVER['HTTP_REFERER'])) {
-        $url = dirname($_SERVER['HTTP_REFERER']) . '/' . basename($_SERVER['PHP_SELF']);
-        trace("alt = $url");
-    }
-    $conf = parse_ini_file("palma.ini", true);
-    $display = $conf['display']['id'];
-    if (isset($conf['display']['ssh'])) {
-        $ssh = $conf['display']['ssh'];
-    }
-    $url = $conf['path']['control_file'];
-    trace("url = $url");
-} else {
-    // Guess configuration.
-    $display = ':1';
-    if (!$unittest[__FILE__]) {
-        $url = dirname($_SERVER['HTTP_REFERER']) . '/' . basename($_SERVER['PHP_SELF']);
-        trace("url = $url");
-    }
-}
-
 function displayCommand($cmd) {
-    global $display;
-    global $ssh;
-    if (isset($ssh)) {
-        $cmd = "$ssh 'DISPLAY=$display $cmd'";
+    if (defined('CONFIG_SSH')) {
+        $cmd = CONFIG_SSH . " 'DISPLAY=" . CONFIG_DISPLAY . " $cmd'";
     } else {
-        $cmd = "DISPLAY=$display HOME=/var/www $cmd";
+        $cmd = "DISPLAY=" . CONFIG_DISPLAY . " HOME=/var/www $cmd";
     }
     $result = shell_exec($cmd);
     trace("cmd=$cmd, result=$result");
@@ -134,14 +110,12 @@ function doLogout($username) {
 }
 
 function clearUploadDir() {
-    global $conf;
-    $upload_dir = $conf['path']['upload_dir'];
-
-    if (is_dir($upload_dir)) {
-        if ($dh = opendir($upload_dir)) {
+    # Remove all files in the upload directory.
+    if (is_dir(CONFIG_UPLOAD_DIR)) {
+        if ($dh = opendir(CONFIG_UPLOAD_DIR)) {
             while (($file = readdir($dh)) !== false) {
                 if ($file != "." AND $file != "..") {
-                    unlink("$upload_dir/$file");
+                    unlink(CONFIG_UPLOAD_DIR . "/$file");
                 }
             }
             closedir($dh);
@@ -519,16 +493,6 @@ if ($unittest[__FILE__]) {
     // Experimental: Get function call from startx.
     parse_str(implode('&', array_slice($argv, 1)), $_GET);
     if (isset($_GET) && count($_GET) > 0) {
-
-        if (file_exists('palma.ini')) {
-            // Get configuration from ini file.
-            $conf = parse_ini_file("palma.ini", true);
-            $display = $conf['display']['id'];
-        } else {
-            // Guess configuration from global PHP variables.
-            $display = ':0';
-        }
-
         foreach ($_GET as $key=>$value) {
             // Only defined actions allowed.
             if ($key == "doLogout") {

download.php
@@ -1,6 +1,9 @@
 <?php
-// this action requires an authorized user
-require_once "auth.php";
+// Copyright (C) 2014-2015 Universitätsbibliothek Mannheim
+// See file LICENSE for license details.
+
+// This action requires an authorized user.
+require_once('auth.php');
 
 // a valid request has to contain a file to be downloaded
 if (!isset($_GET['file']) || empty($_GET['file'])) {
@@ -11,10 +14,10 @@ if (!isset($_GET['file']) || empty($_GET['file'])) {
 // avoid directory traversal vulnerability
 $filename = basename($_GET['file']);
 
-// load location of upload-directory from config
-$conf = parse_ini_file("palma.ini", true);
+// Connect to database and get configuration constants.
+require_once('DBConnector.class.php');
 
-$filepath = $conf['path']['upload_dir'].'/'.$filename;
+$filepath = CONFIG_UPLOAD_DIR . '/' . $filename;
 
 if (file_exists($filepath)) {
   // file exists: return file for download

examples/palma.ini
@@ -5,27 +5,39 @@
 ;
 ; Copy into the same directory as index.php.
 ;
+; The entries here are available as PHP constants:
+,
+; CONFIG_DISPLAY
+; CONFIG_SSH
+; CONFIG_PASSWORD
+; CONFIG_PIN
+; CONFIG_STATIONNAME
+; CONFIG_THEME
+; CONFIG_START_URL
+; CONFIG_CONTROL_FILE
+; CONFIG_UPLOAD_DIR
+;
 
 [display]
-; X display id.
+; X display id (default: ":1").
 id = ":1"
 ; SSH command to connect to display (optional).
 ;~ ssh = "ssh palma@localhost";
 
 [general]
-; Enable or disable password authorization.
+; Enable or disable password authorization (default: true).
 password = false
-; Enable or disable pin authorization.
+; Enable or disable pin authorization (default: true).
 pin = true
-; Name of this PalMA station. Only used for display.
+; Name of this PalMA station (default: host name). Only used for display.
 stationname = "LC 01"
-; Theme (style) to use for this station.
-theme = "ub-mannheim/lc"
+; Theme (style) to use for this station (default: "demo/simple").
+theme = "demo/simple"
 
 [path]
 ; URL which users use to connect.
 start_url = "http://localhost/"
 ; URL used internally for monitor control.
 control_file = "http://localhost/control.php"
-; Directory for file uploads.
-upload_dir = "/var/www/html/uploads"
+; Directory for file uploads (default: "/var/www/html/uploads").
+;upload_dir = "/var/www/html/uploads"

examples/screensaver/tiles.php
@@ -2,7 +2,7 @@
 
 /*
 
-Copyright (C) 2014 Universitätsbibliothek Mannheim
+Copyright (C) 2014-2015 Universitätsbibliothek Mannheim
 See file LICENSE for license details.
 
 Author: Stefan Weil, Universitätsbibliothek Mannheim
@@ -14,19 +14,6 @@ http://www.java2s.com/Code/JavaScript/GUI-Components/AnimationRandomMovement.htm
 http://www.tutorialspoint.com/javascript/javascript_animation.htm
 
 */
-
-$servername = $_SERVER["SERVER_NAME"];
-//~ $serveraddress = $_SERVER["SERVER_ADDR"];
-$serveruri = dirname($_SERVER["REQUEST_URI"]);
-$url = "http://${servername}${serveruri}";
-
-if (file_exists('../../palma.ini')) {
-    // Get configuration from ini file.
-    $conf = parse_ini_file("../../palma.ini", true);
-    if (isset($conf['path']['start_url'])) {
-        $url = $conf['path']['start_url'];
-    }
-}
 
 $pin = sprintf("%04u", rand(0, 9999));
 
@@ -144,9 +131,9 @@ UB Mannheim
 
 <div id="RightSide">
     <span>
-    <img src="../../qrcode/php/qr_img.php?d=<?=$url?>?pin=<?=$pin?>&amp;e=H" alt="QR Code">
+    <img src="../../qrcode/php/qr_img.php?d=<?=CONFIG_START_URL?>?pin=<?=$pin?>&amp;e=H" alt="QR Code">
     <h2>maTeam &ndash; Mannheim Team Monitor / Share your desktop.</h2>
-    <p>Just go to <?=$url?> and enter the PIN.</p>
+    <p>Just go to <?=CONFIG_START_URL?> and enter the PIN.</p>
     <p id="Pin">PIN: <?= $pin ?></p>
     </span>
 </div>

index.php
@@ -59,7 +59,7 @@ Overlays
     $_SESSION['referer'] = 'index.php';
     require('auth.php');
 
-    // Initialise database connection.
+    // Connect to database and get configuration constants.
     require_once('DBConnector.class.php');
     $dbcon = new DBConnector();
 
@@ -77,10 +77,7 @@ Overlays
         $user = $_SERVER['PHP_AUTH_USER'];
     }
 
-    $conf = parse_ini_file("palma.ini", true);
-    $url = $conf['path']['start_url'];
-    $theme = $conf['general']['theme'];
-    $winvnc = "$url/theme/$theme/winvnc-palma.exe";
+    $winvnc = CONFIG_START_URL . "/theme/" . CONFIG_THEME . "/winvnc-palma.exe";
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
        "http://www.w3.org/TR/html4/strict.dtd">
@@ -91,7 +88,7 @@ Overlays
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>PalMA</title>
 
-<link rel="icon" href="theme/<?=$theme?>/favicon.ico" type="image/x-icon">
+<link rel="icon" href="theme/<?=CONFIG_THEME?>/favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 <link rel="stylesheet" href="pure-min.css">
 <link rel="stylesheet" href="palma.css" type="text/css">
@@ -1103,7 +1100,7 @@ pollDatabase();
 <div id="workbench_left">
     <table class="maindisplay" summary="<?=_('Team display')?>">
         <caption>
-            <img id="palma_logo" src="theme/<?=$theme?>/palma-logo-49x18.png"
+            <img id="palma_logo" src="theme/<?=CONFIG_THEME?>/palma-logo-49x18.png"
                  alt="PalMA Logo"> <?=_('Team display')?>
         <?php
               if (isset($_SESSION['monitor'])) {

login.php
@@ -16,24 +16,11 @@
 // proxy based authorization, LDAP, Shibboleth, fixed password).
 // Password authorization can optionally be disabled.
 
-    // Connect to database.
+    // Connect to database and get configuration constants.
     require_once('DBConnector.class.php');
     $dbcon = new DBConnector();
 
     require_once('gettext.php');
-
-    $conf = parse_ini_file("palma.ini", true);
-    $theme = $conf['general']['theme'];
-    if (array_key_exists('password', $conf['general'])) {
-        define('CONFIG_PASSWORD', $conf['general']['password']);
-    } else {
-        define('CONFIG_PASSWORD', true);
-    }
-    if (array_key_exists('pin', $conf['general'])) {
-        define('CONFIG_PIN', $conf['general']['pin']);
-    } else {
-        define('CONFIG_PIN', true);
-    }
 
     $errtext = false;
 
@@ -146,8 +133,8 @@
         $_SESSION['username'] = $username;
         $_SESSION['address'] = $dbcon->ipAddress();
         $_SESSION['pin'] = $pin;
-        $_SESSION['starturl'] = $conf['path']['start_url'];
-        $_SESSION['monitor'] = $conf['general']['stationname'];
+        $_SESSION['starturl'] = CONFIG_START_URL;
+        $_SESSION['monitor'] = CONFIG_STATIONNAME;
         $dbcon->addUser($username, $dbcon->ipAddress(), getDevice());
 
        // Weiterleitung zur geschützten Startseite
@@ -160,7 +147,7 @@
          }
         }
 
-        header('Location: ' . $conf['path']['start_url']);
+        header('Location: ' . CONFIG_START_URL);
         exit;
     }
   }
@@ -175,7 +162,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title><?=_("PalMA &ndash; Login")?></title>
 
-<link rel="icon" href="theme/<?=$theme?>/favicon.ico" type="image/x-icon">
+<link rel="icon" href="theme/<?=CONFIG_THEME?>/favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 <link rel="stylesheet" href="pure-min.css">
 <link rel="stylesheet" href="palma.css">
@@ -199,7 +186,7 @@ TODO:
 
 <fieldset class="login">
     <legend>
-        <img src="theme/<?=$theme?>/palma-logo-67x25.png" alt="PalMA" height="25"/>
+        <img src="theme/<?=CONFIG_THEME?>/palma-logo-67x25.png" alt="PalMA" height="25"/>
         &ndash; <?=_("Login")?>
     </legend>
         <div class="pure-control-group">

commit b0b89f62539a8b9f10470954aa411abac9fdf5fe

SSVNCDaemon: Fix undefined constant name (reported by PhpStorm)
This bug was introduced in commit 18fd7b640e6d2c28dc1c6decf1c1dbbd5ca1fa15.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

SSVNCDaemon.php
@@ -306,7 +306,7 @@ protected function deleteInactiveVncWindow() {
 
             curl_setopt_array($curl, array(
                 CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_URL => CONFIG_CONTROL_FILE_URL .
+                CURLOPT_URL => CONFIG_CONTROL_FILE .
                     '?window=vncwin&delete=VNC&vncid=' . $inactive_win_id,
                 CURLOPT_USERAGENT => 'PalMA cURL Request'
                 ));