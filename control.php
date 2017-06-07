4a54a060b4a1c7a881818ff4566f5ca45338cb11
Dennis Müller <dennis.mueller@bib.uni-mannheim.de>
2017-06-07 12:29:04 +0200

    Fixed syntax error

--- control.php
+++ control.php
@@ -31,7 +31,7 @@ function displayCommand($cmd)
         $cmd = "DISPLAY=" . CONFIG_DISPLAY . " HOME=/var/www $cmd";
     }
     // Tab out of address bar, otherwise controls won't work
-    if $cmd.match(/ midori /) {
+    if ($cmd.match(/ midori /)) {
         $cmd = "$cmd & sleep 3 && xdotool getactiveWindow key Tab";
     }
 
