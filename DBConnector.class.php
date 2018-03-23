98846f40f1597596f0008ec396018d1785316561
Stefan Weil <sw@weilnetz.de>
2018-03-23 21:00:53 +0100

    DBConnector: Report reading errors for palma.ini
    This should fix an issue reported by Scrutinizer.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- DBConnector.class.php
+++ DBConnector.class.php
@@ -289,43 +289,47 @@ function set_constants()
     $conf = parse_ini_file("palma.ini");
     //~ print_r($conf);
 
-    // Entries in group 'display'.
-    if (array_key_exists('id', $conf)) {
-        define('CONFIG_DISPLAY', $conf['id']);
-    }
-    if (array_key_exists('ssh', $conf)) {
-        define('CONFIG_SSH', $conf['ssh']);
-    }
+    if (!$conf) {
+        trace("Error, parsing of palma.ini failed");
+    } else {
+        // Entries in group 'display'.
+        if (array_key_exists('id', $conf)) {
+            define('CONFIG_DISPLAY', $conf['id']);
+        }
+        if (array_key_exists('ssh', $conf)) {
+            define('CONFIG_SSH', $conf['ssh']);
+        }
 
-    // Entries in group 'general'.
-    if (array_key_exists('password', $conf)) {
-        define('CONFIG_PASSWORD', $conf['password']);
-    }
-    if (array_key_exists('pin', $conf)) {
-        define('CONFIG_PIN', $conf['pin']);
-    }
-    if (array_key_exists('stationname', $conf)) {
-        define('CONFIG_STATIONNAME', $conf['stationname']);
-    }
-    if (array_key_exists('theme', $conf)) {
-        define('CONFIG_THEME', $conf['theme']);
-    }
+        // Entries in group 'general'.
+        if (array_key_exists('password', $conf)) {
+            define('CONFIG_PASSWORD', $conf['password']);
+        }
+        if (array_key_exists('pin', $conf)) {
+            define('CONFIG_PIN', $conf['pin']);
+        }
+        if (array_key_exists('stationname', $conf)) {
+            define('CONFIG_STATIONNAME', $conf['stationname']);
+        }
+        if (array_key_exists('theme', $conf)) {
+            define('CONFIG_THEME', $conf['theme']);
+        }
 
-    // Entries in group 'path'.
-    if (array_key_exists('control_file', $conf)) {
-        define('CONFIG_CONTROL_FILE', $conf['control_file']);
-    }
-    if (array_key_exists('policy', $conf)) {
-        define('CONFIG_POLICY', $conf['policy']);
-    }
-    if (array_key_exists('start_url', $conf)) {
-        define('CONFIG_START_URL', $conf['start_url']);
-    }
-    if (array_key_exists('upload_dir', $conf)) {
-        define('CONFIG_UPLOAD_DIR', $conf['upload_dir']);
-    }
-    if (array_key_exists('institution_url', $conf)) {
-        define('CONFIG_INSTITUTION_URL', $conf['institution_url']);
+        // Entries in group 'path'.
+        if (array_key_exists('control_file', $conf)) {
+            define('CONFIG_CONTROL_FILE', $conf['control_file']);
+        }
+        if (array_key_exists('policy', $conf)) {
+            define('CONFIG_POLICY', $conf['policy']);
+        }
+        if (array_key_exists('start_url', $conf)) {
+            define('CONFIG_START_URL', $conf['start_url']);
+        }
+        if (array_key_exists('upload_dir', $conf)) {
+            define('CONFIG_UPLOAD_DIR', $conf['upload_dir']);
+        }
+        if (array_key_exists('institution_url', $conf)) {
+            define('CONFIG_INSTITUTION_URL', $conf['institution_url']);
+        }
     }
 
     // Set default values for constants missing in the configuration file.
