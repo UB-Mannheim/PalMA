86954c47904f3e12641ab0d57a92b55e30270542
Jörg Mechnich <joerg.mechnich@uni-mannheim.de>
2023-10-16 00:54:48 +0200

    Remove minimal php scripts, use php -r instead

--- do_logout_all.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Copyright (C) 2014-2023 Universitätsbibliothek Mannheim
-// See file LICENSE for license details.
-
-require_once 'globals.php';
-debug('do_logout_all.php: begin');
-
-doLogout('ALL');
--- start_ssvnc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Copyright (C) 2014-2023 Universitätsbibliothek Mannheim
-// See file LICENSE for license details.
-
-require_once 'SSVNCDaemon.class.php';
-$ssvnc = new palma\SSVNCDaemon();
